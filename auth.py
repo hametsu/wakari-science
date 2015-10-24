b613b9b35884c554d6026fad7d3a3a42507aeac1
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2015-10-24 15:35:34 +0900

    use oauth2client

--- auth.py
+++ auth.py
@@ -1,58 +1,37 @@
 # coding=utf-8
 import os
-import urllib
 import webbrowser
 
-import requests
+from oauth2client import client
 
 
-CLIENT_ID = '953873850388-atdql57lc2m0v4ngkheb2ppaap7846bo.apps.googleusercontent.com'
-CLIENT_SECRET = 'hnm-HA8_ZMYRVQDTdyg8a_kk'
-SCOPE = 'https://spreadsheets.google.com/feeds'
-REDIRECT_URL = 'https://storage.googleapis.com/arakawatomonori-wakari-web/auth_complete.html'
-SAVE_PATH = './.oauth_token'
+CLIENT_SECRET = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'client_secret.json')
+SAVE_PATH = './.oauth_credentials'
 
 
 def run():
-    query = {
-        'scope': SCOPE,
-        'state': 'something',
-        'redirect_uri': REDIRECT_URL,
-        'response_type': 'code',
-        'client_id': CLIENT_ID,
-        'approval_prompt': 'force',
-        'access_type': 'offline',
-    }
-    params = urllib.urlencode(query)
-    url = 'https://accounts.google.com/o/oauth2/auth?{0}'.format(params)
+    flow = client.flow_from_clientsecrets(
+        CLIENT_SECRET,
+        scope='https://spreadsheets.google.com/feeds',
+        redirect_uri='https://storage.googleapis.com/arakawatomonori-wakari-web/auth_complete.html'
+    )
+    auth_uri = flow.step1_get_authorize_url()
     print('Opening authorization URL, please follow the instructions then copy/paste the "code" param here.')
-    print(url)
-    webbrowser.open_new_tab(url)
+    print(auth_uri)
+    webbrowser.open_new_tab(auth_uri)
 
-    code = raw_input('Code: ')
+    auth_code = raw_input('Code: ')
 
-    payload = {
-        'code': code,
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'redirect_uri': REDIRECT_URL,
-        'grant_type': 'authorization_code',
-    }
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    url = 'https://accounts.google.com/o/oauth2/token'
-    response = requests.post(url, data=payload, headers=headers)
-    response.raise_for_status()
-
-    save(response.content)
-
-    tokens = response.json()
-    print('Access Token: {0}'.format(tokens['access_token']))
-    print('Refresh Token: {0}'.format(tokens['refresh_token']))
+    credentials = flow.step2_exchange(auth_code)
+    save(credentials.to_json())
+    print('Tokens Saved {0}'.format(SAVE_PATH))
 
 
 def save(token_json):
     open(SAVE_PATH, 'w').write(token_json)
-    os.chmod(SAVE_PATH, 0400)
+    os.chmod(SAVE_PATH, 0600)
 
 
 if __name__ == '__main__':
